fe17cc6bb5d1f2ebf4e40ee66bcaebc1d29834d3
mahadibuet11 <1615027@urp.buet.ac.bd>
2023-08-30 15:31:59 +0600

    Code edited by Mahadi

--- app/src/main/java/com/example/myreader/Functions.java
+++ app/src/main/java/com/example/myreader/Functions.java
@@ -2,7 +2,9 @@ package com.example.myreader;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.widget.Toast;
 
@@ -14,12 +16,24 @@ public class Functions {
     public static void createNotification(Context context, String title, String messageBody) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        // Create an explicit intent for launching your app's MainActivity
+        // Create an explicit intent for launching your app's MainActivity
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear the activity stack
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear the activity stack
+
+
         // Create the notification
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setContentTitle(title)
                 .setContentText(messageBody)
                 .setSmallIcon(R.drawable.icon) // Replace with your own icon
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent) // Set the PendingIntent
+                .setAutoCancel(true); // Auto-cancel the notification when clicked
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
